930966695360c9ef61cd330622ca72b1a4cad6e4
Lazar Nikolov <hello@nikolovlazar.com>
2024-08-26 20:07:37 -0400

    vitest node_env fix attempt

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,9 +8,9 @@ export default defineConfig({
       provider: "istanbul",
       reportsDirectory: "./tests/coverage",
     },
-    env: {
-      NODE_ENV: "test",
-    },
+  },
+  define: {
+    "process.env.NODE_ENV": JSON.stringify("test"),
   },
   plugins: [env()],
   resolve: {
