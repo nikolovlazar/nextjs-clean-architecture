eaff5bdee74199a7542b7b38d67cecd07cc9c025
Lazar Nikolov <hello@nikolovlazar.com>
2024-08-26 20:36:22 -0400

    disabling sentry on NODE_ENV test

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -7,6 +7,7 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
+  enabled: process.env.NODE_ENV !== "test",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
