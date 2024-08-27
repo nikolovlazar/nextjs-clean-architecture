6f350bf0a95c56fa1a92382dca93dcbba32f973d
Lazar Nikolov <hello@nikolovlazar.com>
2024-08-26 20:21:59 -0400

    fixing drizzle url

--- drizzle/index.ts
+++ drizzle/index.ts
@@ -5,7 +5,7 @@ import { sessions, todos, users } from "./schema";
 
 // Setup sqlite database connection
 const client = createClient({
-  url: process.env.DATABASE_URL ?? "file://sqlite.db",
+  url: process.env.DATABASE_URL ?? "file:/sqlite.db",
   authToken: process.env.DATABASE_AUTH_TOKEN,
 });
 export const db = drizzle(client, { schema: { users, sessions, todos } });
