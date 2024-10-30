82799a36e5b0dc59226b5a52654419aed4b29960
Lazar Nikolov <hello@nikolovlazar.com>
2024-10-30 13:41:08 -0400

    moved auth use cases into auth module

--- di/modules/authentication.module.ts
+++ di/modules/authentication.module.ts
@@ -3,6 +3,10 @@ import { Container } from '@evyweb/ioctopus';
 import { AuthenticationService } from '@/src/infrastructure/services/authentication.service';
 import { MockAuthenticationService } from '@/src/infrastructure/services/authentication.service.mock';
 
+import { signInUseCase } from '@/src/application/use-cases/auth/sign-in.use-case';
+import { signUpUseCase } from '@/src/application/use-cases/auth/sign-up.use-case';
+import { signOutUseCase } from '@/src/application/use-cases/auth/sign-out.use-case';
+
 import { signInController } from '@/src/interface-adapters/controllers/auth/sign-in.controller';
 import { signOutController } from '@/src/interface-adapters/controllers/auth/sign-out.controller';
 import { signUpController } from '@/src/interface-adapters/controllers/auth/sign-up.controller';
@@ -22,6 +26,29 @@ export function registerAuthenticationModule(container: Container) {
         DI_SYMBOLS.IInstrumentationService,
       ]);
   }
+
+  container
+    .bind(DI_SYMBOLS.ISignInUseCase)
+    .toHigherOrderFunction(signInUseCase, [
+      DI_SYMBOLS.IInstrumentationService,
+      DI_SYMBOLS.IUsersRepository,
+      DI_SYMBOLS.IAuthenticationService,
+    ]);
+
+  container
+    .bind(DI_SYMBOLS.ISignOutUseCase)
+    .toHigherOrderFunction(signOutUseCase, [
+      DI_SYMBOLS.IInstrumentationService,
+      DI_SYMBOLS.IAuthenticationService,
+    ]);
+
+  container
+    .bind(DI_SYMBOLS.ISignUpUseCase)
+    .toHigherOrderFunction(signUpUseCase, [
+      DI_SYMBOLS.IInstrumentationService,
+      DI_SYMBOLS.IAuthenticationService,
+      DI_SYMBOLS.IUsersRepository,
+    ]);
 
   container
     .bind(DI_SYMBOLS.ISignInController)
--- di/modules/users.module.ts
+++ di/modules/users.module.ts
@@ -3,10 +3,6 @@ import { Container } from '@evyweb/ioctopus';
 import { MockUsersRepository } from '@/src/infrastructure/repositories/users.repository.mock';
 import { UsersRepository } from '@/src/infrastructure/repositories/users.repository';
 
-import { signInUseCase } from '@/src/application/use-cases/auth/sign-in.use-case';
-import { signUpUseCase } from '@/src/application/use-cases/auth/sign-up.use-case';
-import { signOutUseCase } from '@/src/application/use-cases/auth/sign-out.use-case';
-
 import { DI_SYMBOLS } from '@/di/types';
 
 export function registerUsersModule(container: Container) {
@@ -20,27 +16,4 @@ export function registerUsersModule(container: Container) {
         DI_SYMBOLS.ICrashReporterService,
       ]);
   }
-
-  container
-    .bind(DI_SYMBOLS.ISignInUseCase)
-    .toHigherOrderFunction(signInUseCase, [
-      DI_SYMBOLS.IInstrumentationService,
-      DI_SYMBOLS.IUsersRepository,
-      DI_SYMBOLS.IAuthenticationService,
-    ]);
-
-  container
-    .bind(DI_SYMBOLS.ISignOutUseCase)
-    .toHigherOrderFunction(signOutUseCase, [
-      DI_SYMBOLS.IInstrumentationService,
-      DI_SYMBOLS.IAuthenticationService,
-    ]);
-
-  container
-    .bind(DI_SYMBOLS.ISignUpUseCase)
-    .toHigherOrderFunction(signUpUseCase, [
-      DI_SYMBOLS.IInstrumentationService,
-      DI_SYMBOLS.IAuthenticationService,
-      DI_SYMBOLS.IUsersRepository,
-    ]);
 }
