7187d72bc414f3996819807155ad3b25be1a20e6
Lazar Nikolov <hello@nikolovlazar.com>
2024-10-30 11:02:31 -0400

    brought back instrumenting the getInjection method

--- di/container.ts
+++ di/container.ts
@@ -1,22 +1,37 @@
-import { Container, createContainer } from '@evyweb/ioctopus';
+import { createContainer } from '@evyweb/ioctopus';
+
+import { DI_RETURN_TYPES, DI_SYMBOLS } from '@/di/types';
+
+import { IInstrumentationService } from '@/src/application/services/instrumentation.service.interface';
 
 import { registerMonitoringModule } from '@/di/modules/monitoring.module';
 import { registerAuthenticationModule } from '@/di/modules/authentication.module';
 import { registerTransactionManagerModule } from '@/di/modules/database.module';
 import { registerTodosModule } from '@/di/modules/todos.module';
 import { registerUsersModule } from '@/di/modules/users.module';
-import { DI_RETURN_TYPES, DI_SYMBOLS } from '@/di/types';
 
-const container: Container = createContainer();
+const ApplicationContainer = createContainer();
 
-registerMonitoringModule(container);
-registerTransactionManagerModule(container);
-registerAuthenticationModule(container);
-registerUsersModule(container);
-registerTodosModule(container);
+registerMonitoringModule(ApplicationContainer);
+registerTransactionManagerModule(ApplicationContainer);
+registerAuthenticationModule(ApplicationContainer);
+registerUsersModule(ApplicationContainer);
+registerTodosModule(ApplicationContainer);
 
 export function getInjection<T extends keyof DI_RETURN_TYPES>(
   key: T
 ): DI_RETURN_TYPES[T] {
-  return container.get<DI_RETURN_TYPES[T]>(DI_SYMBOLS[key]);
+  const instrumentationService =
+    ApplicationContainer.get<IInstrumentationService>(
+      DI_SYMBOLS.IInstrumentationService
+    );
+
+  return instrumentationService.startSpan(
+    {
+      name: '(di) getInjection',
+      op: 'function',
+      attributes: { symbol: key.toString() },
+    },
+    () => ApplicationContainer.get<DI_RETURN_TYPES[T]>(DI_SYMBOLS[key])
+  );
 }
